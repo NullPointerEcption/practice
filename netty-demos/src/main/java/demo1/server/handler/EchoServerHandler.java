c0d92e31b577cd660152e2f24f07ecc3d504d8fc
wyf <1159905228@qq.com>
2019-01-24 21:30:57 +0800

    添加netty工程 加个base-demo

---
+++ netty-demos/src/main/java/demo1/server/handler/EchoServerHandler.java
@@ -0,0 +1,16 @@
+package demo1.server.handler;
+
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+/**
+ * <p>
+ * Description: ServerHandler
+ * </p>
+ *
+ * @author WangYuFei
+ * @date 2019-01-24 21:28
+ **/
+public class EchoServerHandler extends ChannelInboundHandlerAdapter {
+
+
+}
---
+++ netty-demos/src/main/java/demo1/server/server/EchoServer.java
@@ -0,0 +1,12 @@
+package demo1.server.server;
+
+/**
+ * <p>
+ * Description:
+ * </p>
+ *
+ * @author WangYuFei
+ * @date 2019-01-24 21:28
+ **/
+public class EchoServer {
+}
